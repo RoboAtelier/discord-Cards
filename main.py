0ddbe428a38cbbf95ec260c6107d623286164730
Robo <Robo@ROBODESKTOP>
2018-07-23 20:13:52 -0400

    Cleaned unnecessary code

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import discord
 import logging
 import re
 from token import BOT_TOKEN
-from core.commands import cmdconfig, practice, prefix, register
-from core.functions import logwriter, messenger
-from core.mongo import credentials, checkqueries, findqueries
+#from core.commands import cmdconfig, practice, prefix, register
+#from core.functions import logwriter, messenger
+#from core.mongo import credentials, checkqueries, findqueries
 from pymongo import MongoClient, errors
 from os import path
 
@@ -16,6 +16,7 @@ handler = logging.FileHandler(filename='discordverbose.log', mode='a', encoding=
 handler.setFormatter(logging.Formatter(fmt='%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+"""
 # Mongo database connection
 mongo = MongoClient(credentials.HOST, 
     port=credentials.PORT,  
@@ -52,7 +53,7 @@ class LogStore:
 
 @bot.event
 async def on_ready():
-    log = ('Miss Fey has been initialized!'
+    log = ('Cards are stacked and ready!'
         + '\n\tBot name: {}#{}\n\tBot ID: {}\n'.format(bot.user.name, bot.user.discriminator, bot.user.id))
     logwriter.write_log(log, botlog)
 
@@ -141,223 +142,5 @@ async def call_command(message, command, content, logstore):
             elif command in cmdconfig.practice_keywords:
                 await practice.practice_command(message, bot, mongo, content, logstore)
 
-pi = '3.141592653589793'#238462643383279502884197169399375
-e = '2.718281828459045'
-
-def to_postfix(expr):
-    prec = {
-        'sin': 5,
-        'cos': 5,
-        'tan': 5,
-        'sqrt': 5,
-        '^': 4,
-        '*': 3,
-        '/': 3,
-        '+': 2,
-        '-': 2,
-        '(': 1
-    }
-
-    stack = []
-    postfix = []
-    lastchar = ' '
-    number = ''
-    i = 0
-    jump = 1
-
-    while i < len(expr):
-        #'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-        # Scan for numbers
-        if expr[i] in '0123456789.':
-            if lastchar in ')abcdefghijklmnopqrstuvwxyz':
-                stack.append('*')
-            number += expr[i]
-
-        # Scan for variables/keywords
-        elif expr[i] in 'abcdefghijklmnopqrstuvwxyz':
-
-            constant = False
-            function = False
-
-            # Append current number
-            if len(number) > 0:
-                postfix.append(number)
-                number = ''
-
-            # Cosine
-            if expr[i:i+3] == 'cos':
-                stack.append('cos')
-                if expr[i+4] == '(':
-                    jump += 3
-                else:
-                    jump += 2
-                function = True
-
-            # Euler's number e
-            elif expr[i] == 'e':
-                postfix.append(e)
-                constant = True
-
-            # pi π
-            elif expr[i:i+2] == 'pi':
-                postfix.append(pi)
-                jump += 1
-                constant = True
-
-            # Square root √
-            elif expr[i:i+4] == 'sqrt':
-                stack.append('sqrt')
-                if expr[i+5] == '(':
-                    jump += 4
-                else:
-                    jump += 3
-                function = True
-
-            # Sine
-            elif expr[i:i+3] == 'sin':
-                stack.append('sin')
-                if expr[i+4] == '(':
-                    jump += 3
-                else:
-                    jump += 2
-                function = True
-
-            # Tangent
-            elif expr[i:i+3] == 'tan':
-                stack.append('tan')
-                if expr[i+4] == '(':
-                    jump += 3
-                else:
-                    jump += 2
-                function = True
-
-            if constant:
-                if not lastchar in '+-*/^(' or not expr[i+1] in '+-*/^)':
-                    stack.append('*')
-        
-        # Exact symbol match pi π
-        elif expr[i] == 'π':
-
-            # Append current number
-            if len(number) > 0:
-                postfix.append(number)
-                number = ''
-
-            if not lastchar in '+-*/^(' or not expr[i+1] in '+-*/^)':
-                stack.append('*')
-            postfix.append(pi)
-
-        # Exact symbol match square root √
-        elif expr[i] == '√':
-
-            # Append current number
-            if len(number) > 0:
-                postfix.append(number)
-                number = ''
- 
-            stack.append('sqrt')
-
-        # Scan for other operators and other symbols
-        elif not expr[i] == ' ':
-
-            # Append current number
-            if len(number) > 0:
-                postfix.append(number)
-                number = ''
-
-            # Check for negation
-            if expr[i] == '-':
-
-                if lastchar in '+-*/^(':
-                    postfix.append('-1')
-                    stack.append('*')
-
-                # If not negation, add subtraction operator
-                else:
-                    while not len(stack) == 0 and prec[expr[i]] <= prec[stack[len(stack)-1]]:
-                        postfix.append(stack.pop())
-                    stack.append(expr[i])
-
-            # Scan left parenthesis
-            elif expr[i] == '(':
-                if lastchar in '0123456789)':
-                    stack.append('*')
-                stack.append(expr[i])
-
-            # Scan right parenthesis
-            elif expr[i] == ')':
-                top = stack.pop()
-                while not top == '(':
-                    postfix.append(top)
-                    top = stack.pop()
-
-            # Scan for operators
-            else:
-                while not len(stack) == 0 and prec[expr[i]] <= prec[stack[len(stack)-1]]:
-                    postfix.append(stack.pop())
-                stack.append(expr[i])
-
-        if not expr[i] == ' ':
-            if expr[i] in 'abcdefghijklmnopqrstuvwxyz':
-                lastchar = '('
-            else:    
-                lastchar = expr[i]
-        i += jump
-        if jump > 1:
-            jump = 1
-        print(stack)
-        print(postfix)
-    
-    while not len(stack) == 0:
-        postfix.append(stack.pop())
-
-    return postfix
-
-def eval_postfix(postfix):
-    nums = []
-
-    for c in postfix:
-        if re.match(r'\-?\d+', c):
-            nums.append(float(c))
-        else:
-            if c == 'sqrt':
-                result = perform_operation(c, nums.pop(), 0.5)
-                nums.append(result)
-            elif c in ['sin', 'cos', 'tan']:
-                result = perform_operation(c, nums.pop(), 0)
-                nums.append(result)
-            else:
-                num2 = nums.pop()
-                num1 = nums.pop()
-                result = perform_operation(c, num1, num2)
-                nums.append(result)
-    result = nums.pop()
-    if result < 99.9999999999999999:
-        return '{:.15g}'.format(float('{:.15f}'.format(result)))
-    else:
-        return '{:.15g}'.format(result)
-
-def perform_operation(operation, num1, num2):
-    if operation == '+':
-        return num1 + num2
-    elif operation == '-':
-        return num1 - num2
-    elif operation == '*':
-        return num1 * num2
-    elif operation == '/':
-        return num1 / num2
-    elif operation == '^':
-        return num1 ** num2
-    elif operation == 'sqrt':
-        return num1 ** num2
-    elif operation == 'sin':
-        return sin(num1)
-    elif operation == 'cos':
-        return cos(num1)
-    elif operation == 'tan':
-        return tan(num1)
-    else:
-        return 0
-
 bot.run(token)
+"""
